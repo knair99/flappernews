719ea6142f18a06352d457bd232b15246c435e5b
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-27 16:25:14 -0800

    using an angular factory instead of cluttering up our controllers

--- app.js
+++ app.js
@@ -3,23 +3,27 @@
  */
 var app = angular.module('flapperNews', []);
 
+//define factory/service here for our controller
+//mostly to share code and data between different controllers
+app.factory('posts', function(){
+    var o = { posts: [] };
+    return o;
+});
+
+//Now define our controller
+//Now inject that factory into our controller
 app.controller('MainCtrl', [
     '$scope',
-    function($scope){
+    'posts', //here
+    function($scope, posts){
         $scope.test = 'Hello world!';
 
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];
+        $scope.posts = posts.posts;
 
         $scope.addPost = function(){
             if($scope.title === '') {return;}
 
-            $scope.posts.push( { title: $scope.title, link: $scope.link, upvotes: 4});
+            $scope.posts.push( { title: $scope.title, link: $scope.link, upvotes: 0});
             $scope.title = "";
             $scope.link = "";
         }
