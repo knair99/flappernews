dc7d23f5c33f24ca81f9825a154e35c969781f46
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-27 19:27:56 -0800

    'posts' page that has comments - another controller, another router, another template, another function for adding comments(in the controller), use of {{index}}

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.config([
     function($stateProvider, $urlRouterProvider){
         //For the /home requests, route it to a custom template, and use MainCtrl controller
         $stateProvider.state('home', {url:'/home', templateUrl:'/home.html', controller:'MainCtrl'});
+
+        //Now route the posts page itself
+        $stateProvider.state('posts', {url:'/posts/{id}', templateUrl:'/posts.html', controller:'PostCtrl'});
+
         //For everything else, route to home, for now
         $urlRouterProvider.otherwise('home');
     }
@@ -24,7 +28,7 @@ app.factory('posts', function(){
     return o;
 });
 
-//Now define our controller
+//Now define our controller for home page
 //Now inject that factory into our controller
 app.controller('MainCtrl', [
     '$scope',
@@ -37,18 +41,36 @@ app.controller('MainCtrl', [
         $scope.addPost = function(){
             if($scope.title === '') {return;}
 
-            $scope.posts.push( { title: $scope.title, link: $scope.link, upvotes: 0});
+            $scope.posts.push( { title: $scope.title, link: $scope.link, upvotes: 0, comments:[]});
             $scope.title = "";
             $scope.link = "";
         }
 
         $scope.incrementUpvotes = function(post){
-            console.log("upvoted");
             post.upvotes += 1;
         }
 }]);
 
+//define a controller for our posts
+app.controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
 
+        $scope.addComment = function(){
+            if($scope.body === '') {return;}
+
+            //First, get the relevant post
+            $scope.post = posts.posts[$stateParams.id];
+
+            //Then add a comments element into a comments array, on the post object
+            //A comment looks like this comments = [ {body:sometext, author:text, upvotes:0}]
+            $scope.post.comments.push( { body: $scope.body, author: 'user', upvotes: 0} );
+            $scope.body = "";
+        }
+    }
+]);
 
 //Notes:
 //1. You can spin off a simple web server for testing by typing 'python -m SimpleHTTPServer 8888'
